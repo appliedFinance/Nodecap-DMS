7fd864a45769128d030b0ff350ad3edb37c8b6b8
appliedFinance <atlusr@yahoo.com>
2018-06-23 17:38:54 -0400

    Initial Commit

---
+++ test/test-hitup.js
@@ -0,0 +1,29 @@
+chai = require('chai');
+const chaiHttp = require('chai-http');
+
+
+const {app, runServer, closeServer} = require('../server');
+const { PORT, TEST_DATABASE_URL } = require('../config');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Users', function() {
+	before(function() {
+		return runServer(TEST_DATABASE_URL);
+	});
+
+	after(function() {
+		return closeServer();
+	});
+	it('should list users on GET', function() {
+		return chai.request(app)
+			.get('/')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.json;
+			});
+	});
+});
+
