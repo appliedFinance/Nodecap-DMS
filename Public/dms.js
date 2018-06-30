0e4870e9dfdd7ba01ed3de45790c55a30dab7a81
appliedFinance <atlusr@yahoo.com>
2018-06-30 19:33:41 -0400

    Menu branch inital commit - updated the html in the static index.html file - added the cool background image - implement the javascript Menu Pulldown code - added all supporting .css stuff

--- Public/dms.js
+++ Public/dms.js
@@ -1,113 +1,31 @@
+// Client side 
+const say = console.log;
 
-let MOCK_EMPLOYEED_DATA = {
-	"empolyees": [
-	{
-		"_id": "11111",
-		"name": {
-			"firstName": "Tom",
-			"middleName": "A",
-			"lastName": "Jones"
-		},
-		"type": "W2",
-		"address": {
-			"street1": "123 Apple Street",
-			"city": "Marietta",
-			"state": "GA",
-			"zipcode": "30068"
-		},
-		"phone": "(770) 111-3333",
-		"email": "tom@yahoo.com",
-		"otherInfo": "",
-		"payRate": "20.00",
-		"OTPayRate": "30.00",
-		"billRate": "25.00",
-		"OTBillRate": "32.0",
-		"paidHolidays": "no",
-//		"startDate": ,
-//		"endDate": ,
-		"length": "3 mo"
-	},
-	{
-		"_id": "22222",
-		"name": {
-			"firstName": "Buffy",
-			"middleName": "",
-			"lastName": "Summers"
-		},
-		"type": "W2",
-		"address": {
-			"street1": "123 Side Road",
-			"city": "Sunnyville",
-			"state": "CA",
-			"zipcode": "90202"
-		},
-		"phone": "(770) 555-8888",
-		"email": "bsum@gmail.com",
-		"otherInfo": "",
-		"payRate": "30.00",
-		"OTPayRate": "40.00",
-		"billRate": "35.00",
-		"OTBillRate": "42.0",
-		"paidHolidays": "no",
-//		"startDate": ,
-//		"endDate": ,
-		"length": "open"
-	},
-	{
-		"_id": "33333",
-		"name": {
-			"firstName": "Yog",
-			"middleName": "So",
-			"lastName": "Thoth"
-		},
-		"type": "W2",
-		"address": {
-			"street1": "999 Main Streen",
-			"city": "St. Louis",
-			"state": "MI",
-			"zipcode": "48820"
-		},
-		"phone": "(770) 222-0000",
-		"email": "iamyog@hotmail.com",
-		"otherInfo": "",
-		"payRate": "50.00",
-		"OTPayRate": "75.00",
-		"billRate": "99.00",
-		"OTBillRate": "99.0",
-		"paidHolidays": "no",
-//		"startDate": ,
-//		"endDate": ,
-		"length": "6 mo"
-	}
-	]
-};
 
-function getRecentStatusUpdates(callbackFn) {
-	setTimeout(function(){ callbackFn(MOCK_EMPLOYEED_DATA)}, 100);
-}
+function load_MainMenu() {
+	say("+ load_MainMenu");
 
-function displayStatusUpdates(data) {
-	for (index in data) {
-		$('.output').append(
-				'<p>' + data[index].text + '</p>');
-	}
+	$('.js-main-menu').html(`
+		<div class="dropdown">
+		<button class="dropbtn">Active Work</button>
+		<div class="dropdown-content">
+		<a href="#">Employees</a>
+		<a href="#">OnSite</a>
+		<a href="#">Invoices</a>
+		</div>
+		</div>
+		`);
+
+	say("- load_MainMenu");
 }
 
 
 function watcher() {
+	say("+ watcher");	
+	load_MainMenu();
 
-	console.log("Starting:");
+	say("- watcher");
+};
 
-	$('form').submit(event=>{ 
-		event.preventDefault();
-		$('.output').html("HI");
-		let line= $('input[type="text"]').val();
-		// Some basic error checking of the input line:
-		peeps = line.split(/[;,]+/);
-		peeps = peeps.filter(Boolean); // remove newlines & spaces
-		peeps = Array.from(new Set(peeps)); // remove duplicates
-		displayStatusUpdates(MOCK_EMPLOYEED_DATA);
-	});
-}
 
 $(watcher);
