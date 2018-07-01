b29c79064742cb33dd37184c05e3690532f42345
appliedFinance <atlusr@yahoo.com>
2018-07-01 15:55:35 -0400

    Simple Directory mock-up working - each cell is a 6 by 6 grid - 3 buttons added

--- Public/dms.js
+++ Public/dms.js
@@ -1,21 +1,34 @@
 // Client side 
 const say = console.log;
 
-function load_MockData() {
-	$('.js-data').html(`
-		<div class="a-test">
-			<p>Some text some text some text some text..</p>
-			<p>Some text some text some text some text..</p>
-			<p>Some text some text some text some text..</p>
-			<p>Some text some text some text some text..</p>
-			<p>Some text some text some text some text..</p>
+function renderAADirItem(name,type,startDate) {
+	let s = `
+		<div class="a-dir">
+		<div class="row">
+		<div class="col-6">
+		<p>${name} -- ${type} -- ${startDate} thru _</p>
 		</div>
+		<div class="col-6 dir-button">
+		<button>Cool-thing</button>
+		<button>Edit</button>
+		<button>Print</button>
+		</div>
+		</div>
+		</div>
+		`;
+	return s;
+}
 
-		<div class="a-test">
-			<p>Some text some text some text some text..</p>
-			<p>Some text some text some text some text..</p>
-		</div>
-		`);
+function load_MockData() {
+	$('.js-data').html("");
+	for(let i=0; i<20; i++) {
+		let name = "Nugent, Ted";
+		let type = "W2";
+		let startDate = "3/15/18";
+		let elt = renderAADirItem(name,type,startDate);
+		say(elt);
+		$('.js-data').append(elt);
+	}
 }
 
 function load_MainMenu() {
